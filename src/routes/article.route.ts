faa4a2953ec2db9153861ef5179365f9a38601b3
Omeiza Owuda <owudaomeiza@gmail.com>
2023-07-21 03:42:13 +0100

    Update for new UI

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,34 +1,20 @@
-import {CategoryTypes} from "../types/category";
 import express, { Request, Response } from "express";
+import { ArticleTypes } from "../types/article";
 const router = express.Router();
 import ArticleModel from "../models/article.model";
-import CategoryModel from "../models/category.model";
 
 router.get("/", async (req: Request, res: Response): Promise<Response> => {
-	const result: any = await CategoryModel
-		.find({}, null, {})
-		.select({ "_id": 0, "__v": 0, "createdAt": 0, "updatedAt": 0})
+	const per_page: any = req.query && req.query.count ? req.query.count : 48;
+	const page: any = req.query.page && req.query.page ? req.query.page : 1;
+	const args = {limit: per_page, skip: per_page * (page - 1), sort: { articleDate: -1 }};
+
+	const result: ArticleTypes[] = await ArticleModel
+		.find({}, null, args)
+		.select({ "_id": 0, "__v": 0 })
 		.lean()
 		.exec();
 
-	if (result && result.length > 0) {
-		const all: CategoryTypes[] = [];
-		const categories: CategoryTypes[] = result;
-		const per_page: any = req.query && req.query.count ? req.query.count : 48;
-		const args = {limit: per_page, sort: {articleDate: -1}};
-
-		for (const category of categories) {
-			category.articles = await ArticleModel
-				.find({category: category.name}, null, args)
-				.select({"_id": 0, "__v": 0})
-				.exec();
-
-			all.push(category);
-		}
-
-		return res.json(all);
-	}
-
+	if (result && result.length > 0) return res.json(result);
 	return res.status(404).json({
 		status: "No record found"
 	});
