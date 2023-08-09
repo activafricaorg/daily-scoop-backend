5efb3681afbefd2a669f5a85ea6bc4315408b74a
Omeiza Owuda <owudaomeiza@gmail.com>
2023-08-09 23:54:59 +0100

    Mongo connection string fix

--- src/configs/db.configs.ts
+++ src/configs/db.configs.ts
@@ -5,6 +5,6 @@
 
 // const env = process.env;
 module.exports = {
-	uri: process.env.MONGO_URI ?? "mongodb+srv://omeiza:avxt9g8pX5g8V224@cluster0.diih52x.mongodb.net/?retryWrites=true&w=majority",
+	uri: process.env.MONGO_URI ?? "mongodb+srv://omeiza:avxt9g8pX5g8V224@cluster0.diih52x.mongodb.net",
 	database: process.env.MONGO_DB ?? "dailyscoop"
 }
