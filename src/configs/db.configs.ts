2ad42684af6ca1f5686e6d7e469a1beead585c7b
Omeiza Owuda <owudaomeiza@gmail.com>
2024-01-26 18:50:30 +0100

    Update before test

--- src/configs/db.configs.ts
+++ src/configs/db.configs.ts
@@ -1,8 +1,3 @@
-/**
- * Database configuration
- * Author: https://github.com/omeiza
- */
-
 module.exports = {
 	uri: process.env.MONGO_URI ?? "mongodb+srv://omeiza:avxt9g8pX5g8V224@cluster0.diih52x.mongodb.net",
 	database: process.env.MONGO_DB ?? "dailyscoop"
